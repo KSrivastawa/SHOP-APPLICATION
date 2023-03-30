4f7f933d270a09117a0e544855f2c16f6948af30
KSrivastawa <kt.ketansri@gmail.com>
2023-03-30 09:39:31 +0530

    updated

--- USER-SERVICE/src/main/java/com/shop/config/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.shop.config;
-
-public class SecurityConstants {
-
-    public static final String JWT_KEY = "secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
-    public static final String JWT_HEADER = "Authorization";
-
-}
