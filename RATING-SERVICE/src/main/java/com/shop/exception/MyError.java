587d9966bef1f15b97c650542d02a4c1da77fbcf
KSrivastawa <kt.ketansri@gmail.com>
2023-03-30 09:41:51 +0530

    Rating Service

--- RATING-SERVICE/src/main/java/com/shop/exception/MyError.java
+++ RATING-SERVICE/src/main/java/com/shop/exception/MyError.java
@@ -1,14 +1,18 @@
 package com.shop.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyError {
 
     private String message;
     private String description;
-    private LocalDateTime dateTime;
+    private LocalDateTime timestamp;
 
 }
